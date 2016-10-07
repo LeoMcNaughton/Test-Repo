894c72fbc407383b55796f91c769fdc38584064c
LeoMcNaughton <9lmcnaughton@stpatrick.org>
2016-10-07 10:26:38 -0500

    Today I created three new Octocats including the Emperor, Kaiser, and Prince

--- octocats.js
+++ octocats.js
@@ -1,8 +1,15 @@
 //Write your constructor function below.
 //You should have 2 instance variables and 1 instance function included.
+function octocat(n,a){
+this.name = n
+this.arms = a
+this.slap = function Slap(){
 
-
-
+for(var i=0;i<this.arms;i++){
+  console.log("SLAP")
+}
+}
+}
 
 
 
@@ -12,3 +19,6 @@
 
 
 //Write your 3 new octocat objects below here.
+var kaisercat = new octocat ("William",66)
+var Emperor = new octocat ("Harry",99)
+var Prince = new octocat ("Jaffar",2)
